e02bbffc8bfaac0235822ec4d0ef0821341da6d3
YukihiroMoriyama <m.y.like.music.10@gmail.com>
2016-02-13 19:05:25 +0900

    make kutouten animation

--- js/main.js
+++ js/main.js
@@ -1,28 +1,64 @@
 $(document).on('pageinit', '#reader', function() {
     $.get('./data/sample.txt', function(data){
-        var texts = data.split(/\r\n|\r|\n/);
-        var start = 2;
+        // var texts = data.split(/\r\n|\r|\n/);
+        var texts = data.split("。");
+        var text = ""
+        var start = 0;
         var end = texts.length;
 
-        $(document).find('p').html(texts[start]);
+        showText();
 
         $(document).on('touchstart', '#reader ons-back-button', offEvent);
         $(document).on('touchstart', 'p', requestTextFeature);
 
+        function showText() {
+            text = ""
+            for (var i = 0; i < 4; i++) {
+                text += texts[start] + "。";
+                start++;
+            }
+
+            var result = "";
+
+            tmp = text.split('。');
+            for (var i = 0; i < tmp.length; i++) {
+                if (i !== tmp.length - 1) {
+                    result = result + (tmp[i] + "<div class='ten'>。</div>");
+                }
+            }
+
+            var tmp = result.split('、');
+            console.log(tmp);
+            for (var i = 0; i < tmp.length; i++) {
+                if (i !== tmp.length - 1) {
+                    result = result + (tmp[i] + "<div class='ten'>、</div>");
+                }
+            }
+
+            $(document).find("p").html(result);
+        }
+
         function requestTextFeature() {
             var $request = $.ajax({
-                type: 'post',
+                type: 'get',
                 url: 'https://yomuu-api.herokuapp.com/textFeature',
                 data: {
-                    'text': texts[start]
+                    'text': text
                 }
             });
 
             $request.done(function(res) {
                 console.log(res);
-                start++;
-                if (start >= end) return;
-                $(document).find("p").html(texts[start]);
+
+                TweenMax.staggerTo(".ten", 0.5, {y:200}, 0.5, showText);
+                // TweenMax.to(".ten", 0.5, {
+                //   y: "+=300"
+                // },);
+
+                // $(document).find('.ten').each(function(index.element) {
+                //     var offset = $(element).offset();
+                //     console.log("left = " + offset.left + ", top = " + offset.top);
+                // });
             });
 
             $request.fail(function(xhr, status, error) {
